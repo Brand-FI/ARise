ff48b93c3c902961b860a856917f7c2c4acbd19e
Brand-FI <ferdinand.brandon@gmail.com>
2026-04-29 18:03:40 +0700

    delete script unused

--- Assets/Scripts/AppState.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AppState", menuName = "Scriptable Objects/AppState")]
-public class AppState : ScriptableObject
-{
-    public int selectedAnimalID;
-}
